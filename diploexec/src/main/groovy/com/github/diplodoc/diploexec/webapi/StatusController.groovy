c0f8cdd55f86f75b865e7718fce075663e6b421a
yaroslav.yermilov <yermilov17@gmail.com>
2015-01-05 17:47:02 +0200

    DIPLODOC-21. Diploexec RESTful API has wrong layout

---
+++ diploexec/src/main/groovy/com/github/diplodoc/diploexec/webapi/StatusController.groovy
@@ -0,0 +1,27 @@
+package com.github.diplodoc.diploexec.webapi
+
+import com.github.diplodoc.diploexec.DiploflowsRuntimeEnvironment
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
+
+/**
+ * @author yaroslav.yermilov
+ */
+@Controller
+@RequestMapping('/api/v1')
+class StatusController {
+
+    @Autowired
+    DiploflowsRuntimeEnvironment runtime
+
+    @RequestMapping(value='/status', method=RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody def status() {
+        runtime.status()
+    }
+}
