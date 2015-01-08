03067d0867039ee79717420a6465b5ea44a0f786
yaroslav.yermilov <yermilov17@gmail.com>
2015-01-08 15:28:46 +0200

    DIPLODOC-31. Move REST clients to HATEOAS

--- diplobase/src/main/groovy/com/github/diplodoc/diplobase/client/ModuleClient.groovy
+++ diplobase/src/main/groovy/com/github/diplodoc/diplobase/client/ModuleClient.groovy
@@ -1,8 +1,18 @@
 package com.github.diplodoc.diplobase.client
 
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.github.diplodoc.diplobase.domain.diploexec.Module
 import com.sun.org.apache.xpath.internal.operations.Mod
 import groovy.json.JsonSlurper
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.hateoas.MediaTypes
+import org.springframework.hateoas.Resource
+import org.springframework.hateoas.Resources
+import org.springframework.hateoas.hal.Jackson2HalModule
+import org.springframework.http.HttpMethod
+import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
 
 /**
@@ -10,21 +20,23 @@ import org.springframework.web.client.RestTemplate
  */
 class ModuleClient {
 
+    private final static MODULES = new ParameterizedTypeReference<Resources<Resource<Module>>>() {}
+
     String rootUrl
 
-    RestTemplate restTemplate = new RestTemplate()
-    JsonSlurper jsonSlurper = new JsonSlurper()
+    RestTemplate hateoasTemplate = newHateoasTemplate()
 
     ModuleClient(String rootUrl) {
         this.rootUrl = rootUrl
     }
 
     List<Module> modules() {
-        def modulesJson = jsonSlurper.parseText(restTemplate.getForObject("${rootUrl}/diplobase/modules", String))
-        List modulesLinks = (modulesJson.links as List).findAll { link -> link.rel == 'module' }
-
-        modulesLinks.collect { moduleLink ->
-            jsonToModule(jsonSlurper.parseText(restTemplate.getForObject(moduleLink.href, String)))
+        ResponseEntity<Resources<Resource<Module>>> response = hateoasTemplate.exchange("${rootUrl}/diplobase/modules", HttpMethod.GET, null, MODULES)
+        response.body.collect { Resource<Module> resource ->
+            Module module = resource.content
+            String selfHref = resource.id.href
+            module.id = Long.parseLong(selfHref.substring(selfHref.lastIndexOf('/') + 1))
+            return module
         }
     }
 
@@ -50,5 +62,24 @@ class ModuleClient {
         String definition = moduleJson.definition
 
         new Module(id: id, name: name, definition: definition)
+    }
+
+    private static RestTemplate newHateoasTemplate() {
+        RestTemplate restTemplate = new RestTemplate()
+
+        restTemplate.messageConverters.removeAll { messageConverter ->
+            messageConverter instanceof MappingJackson2HttpMessageConverter
+        }
+
+        ObjectMapper objectMapper = new ObjectMapper()
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        objectMapper.registerModule(new Jackson2HalModule())
+        MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter()
+        messageConverter.supportedMediaTypes = [ MediaTypes.HAL_JSON ]
+        messageConverter.objectMapper = objectMapper
+
+        restTemplate.messageConverters << messageConverter
+
+        return restTemplate
     }
 }
