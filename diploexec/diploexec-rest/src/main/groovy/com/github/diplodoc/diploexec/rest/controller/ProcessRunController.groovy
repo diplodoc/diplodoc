2743fb668eaede0ac1f49a93d8109c7042f6a8e9
yaroslav.yermilov <yermilov17@gmail.com>
2015-01-09 16:22:56 +0200

    DIPLODOC-35. Move diploexec RESTful API to HATEOAS

--- diploexec/diploexec-rest/src/main/groovy/com/github/diplodoc/diploexec/rest/controller/ProcessRunController.groovy
+++ diploexec/diploexec-rest/src/main/groovy/com/github/diplodoc/diploexec/rest/controller/ProcessRunController.groovy
@@ -3,6 +3,9 @@ package com.github.diplodoc.diploexec.rest.controller
 import com.github.diplodoc.diplobase.domain.diploexec.ProcessRun
 import com.github.diplodoc.diploexec.Diploexec
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.hateoas.Resource
+import org.springframework.hateoas.ResourceProcessor
+import org.springframework.hateoas.mvc.ControllerLinkBuilder
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.PathVariable
@@ -17,15 +20,20 @@ import org.springframework.web.bind.annotation.ResponseStatus
  */
 @Controller
 @RequestMapping('/api/v1')
-class ProcessRunController {
+class ProcessRunController implements ResourceProcessor<Resource<ProcessRun>> {
 
     @Autowired
     Diploexec diploexec
 
     @RequestMapping(value='/process/run', method=RequestMethod.POST)
     @ResponseStatus(HttpStatus.CREATED)
-    @ResponseBody ProcessRun run(@RequestBody ProcessRun processRun) {
-        
+    @ResponseBody Resource<ProcessRun> run(@RequestBody ProcessRun processRun) {
         assert false : 'not implemented yet'
+    }
+
+    @Override
+    Resource<ProcessRun> process(Resource<ProcessRun> resource) {
+        resource.add(ControllerLinkBuilder.linkTo(ProcessRunController).slash('process').slash('run').withSelfRel())
+        return resource
     }
 }
