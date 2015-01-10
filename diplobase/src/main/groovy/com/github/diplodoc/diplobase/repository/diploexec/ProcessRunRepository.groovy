b3ab23cf2dc3b748112d4f3c6ac8073cb68a977e
yaroslav.yermilov <yermilov17@gmail.com>
2015-01-10 14:34:33 +0200

    save process run entity to database

---
+++ diplobase/src/main/groovy/com/github/diplodoc/diplobase/repository/diploexec/ProcessRunRepository.groovy
@@ -0,0 +1,9 @@
+package com.github.diplodoc.diplobase.repository.diploexec
+
+import com.github.diplodoc.diplobase.domain.diploexec.ProcessRun
+import org.springframework.data.repository.CrudRepository
+
+/**
+ * @author yaroslav.yermilov
+ */
+interface ProcessRunRepository extends CrudRepository<ProcessRun, Long> {}
--- diploexec/src/main/groovy/com/github/diplodoc/diploexec/Diploexec.groovy
+++ diploexec/src/main/groovy/com/github/diplodoc/diploexec/Diploexec.groovy
@@ -1,13 +1,19 @@
 package com.github.diplodoc.diploexec
 
+import com.github.diplodoc.diplobase.client.ProcessRunDataClient
 import com.github.diplodoc.diplobase.domain.diploexec.ProcessRun
+import org.springframework.beans.factory.annotation.Autowired
 
 /**
  * @author yaroslav.yermilov
  */
 class Diploexec {
 
+    @Autowired
+    ProcessRunDataClient processRunDataClient
+
     ProcessRun run(ProcessRun processRun) {
+        processRunDataClient.create processRun
         assert false : 'not implemented yet'
     }
 }
