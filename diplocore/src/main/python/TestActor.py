127a7d3e64753a761c94772fafaa855b3a7276c1
yaroslav.yermilov <yermilov17@gmail.com>
2015-01-31 17:25:08 +0200

    JythonTestModule tests

--- diplocore/src/main/python/TestActor.py
+++
@@ -1,6 +0,0 @@
-from com.github.diplodoc.diplocore.modules import TestActor
-
-class TestActor(TestActor):
-    def modify(self, object):
-        object.setProperty('data', 'just testing...')
-        return object
