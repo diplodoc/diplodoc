76c7e068b542dced99a776b3bb00a3315b2123dd
yaroslav.yermilov <yermilov17@gmail.com>
2015-01-31 16:57:13 +0200

    PostLoader test

---
+++ diplocore/src/test/groovy/com/github/diplodoc/diplocore/modules/PostLoaderSpecs.groovy
@@ -0,0 +1,40 @@
+package com.github.diplodoc.diplocore.modules
+
+import com.github.diplodoc.diplobase.domain.diplodata.Post
+import com.github.diplodoc.diplobase.domain.diplodata.Source
+import com.github.diplodoc.diplobase.repository.diplodata.PostRepository
+import com.github.diplodoc.diplocore.services.Web
+import org.jsoup.nodes.Document
+import spock.lang.Specification
+
+/**
+ * @author yaroslav.yermilov
+ */
+class PostLoaderSpecs extends Specification {
+
+    Web web = Mock(Web)
+    PostRepository postRepository = [ save : { Post post -> post.id = 1; return post } ] as PostRepository
+    PostLoader postLoader = new PostLoader(web: web, postRepository: postRepository)
+
+    def 'load post'() {
+        when:
+            Source source = new Source()
+            String url = 'url'
+
+            Document document = Mock(Document)
+            document.html() >> 'html'
+            web.load(url) >> document
+
+        then:
+            Post actual = postLoader.loadPost(source, url)
+
+        expect:
+            actual.html == 'html'
+            actual.title == null
+            actual.id == 1
+            actual.meaningText == null
+            actual.source == source
+            actual.type == null
+            actual.url == 'url'
+    }
+}
