422d634819626521612f543a82e8b832d8393e48
yaroslav.yermilov <yermilov17@gmail.com>
2015-02-24 02:51:02 +0200

    mock method

--- diploexec/diploexec-test/src/main/groovy/com/github/diplodoc/diploexec/test/ProcessTest.groovy
+++ diploexec/diploexec-test/src/main/groovy/com/github/diplodoc/diploexec/test/ProcessTest.groovy
@@ -1,16 +1,19 @@
 package com.github.diplodoc.diploexec.test
 
 import com.github.diplodoc.diplobase.domain.jpa.diploexec.Process
+import groovy.util.logging.Slf4j
 
 /**
  * @author yaroslav.yermilov
  */
+@Slf4j
 class ProcessTest {
 
     Process process
     DiploexecTest diploexecTest
 
     Process processUnderTest
+    Map<String, String> mockedModules = [:]
 
     ProcessTest(Process process, DiploexecTest diploexecTest) {
         this.process = process
@@ -21,7 +24,7 @@ class ProcessTest {
         try {
             new GroovyShell(binding()).evaluate(process.definition)
         } catch (e) {
-            assert null : 'not implemented yet'
+            assert null : "not implemented yet: $e"
         }
 
         assert null : 'not implemented yet'
@@ -32,6 +35,14 @@ class ProcessTest {
 
         binding.test = { String processUnderTestName ->
             processUnderTest = diploexecTest.getProcess(processUnderTestName)
+
+            println "Process under test ${processUnderTest.name}"
+        }
+
+        binding.mock = { Map param ->
+            mockedModules[param.module] = param.by
+
+            println "Module ${param.module} will be mocked by ${param.by}"
         }
 
         return binding
