12ed76b60fcfaa7af3e1a93ec8842997338156ed
yaroslav.yermilov <yermilov17@gmail.com>
2015-03-02 14:10:30 +0200

    integrate java and python part of post classifier

--- diplocore/src/main/groovy/com/github/diplodoc/diplocore/modules/PostTypeClassifier.groovy
+++ diplocore/src/main/groovy/com/github/diplodoc/diplocore/modules/PostTypeClassifier.groovy
@@ -1,10 +1,17 @@
 package com.github.diplodoc.diplocore.modules
 
+import com.github.diplodoc.diplobase.domain.jpa.diploexec.ProcessRun
 import com.github.diplodoc.diplobase.domain.mongodb.Post
 import com.github.diplodoc.diplobase.repository.mongodb.PostRepository
+import groovy.json.JsonBuilder
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
 import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
 import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
 
 /**
  * @author yaroslav.yermilov
@@ -16,6 +23,9 @@ class PostTypeClassifier implements Bindable {
     @Autowired
     PostRepository postRepository
 
+    @Autowired
+    RestTemplate restTemplate
+
     @Override
     void bindSelf(Binding binding) {
         binding.classify = { Post post -> classify(post) }
@@ -26,13 +36,14 @@ class PostTypeClassifier implements Bindable {
 
         post = postRepository.findOne(post.id)
 
-        if (post.meaningText.length() > 6000) {
-            post.type = 'ARTICLE'
-        } else {
-            post.type = 'NEWS'
-        }
+        def request = JsonOutput.toJson(text: post.meaningText)
+
+        def response = new JsonSlurper().parseText(restTemplate.exchange('localhost:5050/classify', HttpMethod.POST, new HttpEntity<>(request), String.class))
+
+        post.type = response.label
 
         post = postRepository.save post
+
         log.debug('post {} classified as {}', post.url, post.type)
         return post
     }
