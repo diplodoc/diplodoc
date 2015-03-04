8dfdc8c14427f3bb826853f154560c9d3c4e62da
yaroslav.yermilov <yermilov17@gmail.com>
2015-03-04 16:04:23 +0200

    DIPLODOC-108. Move Data.Sources module to WS

--- diplocore/src/main/groovy/com/github/diplodoc/diplocore/modules/data/Sources.groovy
+++ diplocore/src/main/groovy/com/github/diplodoc/diplocore/modules/data/Sources.groovy
@@ -4,23 +4,27 @@ import com.github.diplodoc.diplobase.domain.mongodb.Source
 import com.github.diplodoc.diplobase.repository.mongodb.SourceRepository
 import com.github.diplodoc.diplocore.modules.Bindable
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
 
 /**
  * @author yaroslav.yermilov
  */
-@Component('data.sources')
-class Sources implements Bindable {
+@Controller
+@RequestMapping('/data/sources')
+class Sources {
 
     @Autowired
     SourceRepository sourceRepository
 
-    @Override
-    void bindSelf(Binding binding) {
-        binding.findAllSources = this.&findAll
-    }
-
-    Collection<Source> findAll() {
+    @RequestMapping(value = '/', method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody Collection<Source> '/'() {
         sourceRepository.findAll()
     }
 }
