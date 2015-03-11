3a6bee9e581ec309b440ef362c967e74a7f37a9d
yaroslav.yermilov <yermilov17@gmail.com>
2015-03-12 01:16:15 +0200

    ProcessRunController tests

---
+++ diploexec/src/test/groovy/com/github/diplodoc/diploexec/controller/ProcessRunControllerSpec.groovy
@@ -0,0 +1,36 @@
+package com.github.diplodoc.diploexec.controller
+
+import com.github.diplodoc.diplobase.domain.jpa.diploexec.ProcessRun
+import com.github.diplodoc.diplobase.domain.jpa.diploexec.ProcessRunParameter
+import com.github.diplodoc.diploexec.Diploexec
+import spock.lang.Specification
+
+/**
+ * @author yaroslav.yermilov
+ */
+class ProcessRunControllerSpec extends Specification {
+
+    Diploexec diploexec = Mock(Diploexec)
+
+    ProcessRunController processRunController = new ProcessRunController(diploexec: diploexec)
+
+    def 'void run(ProcessRun processRun)'() {
+        when:
+            ProcessRun processRun = new ProcessRun(id : 1)
+            processRun.parameters = [
+                new ProcessRunParameter(key: 'key-1', value: 'value-1'),
+                new ProcessRunParameter(key: 'key-2', value: 'value-2')
+            ]
+
+            processRunController.run(processRun)
+
+        then:
+            1 * diploexec.run({ arg ->
+                arg.id == 1 &&
+                arg.parameters == [
+                    new ProcessRunParameter(key: 'key-1', value: 'value-1', processRun: arg),
+                    new ProcessRunParameter(key: 'key-2', value: 'value-2', processRun: arg)
+                ]
+            })
+    }
+}
