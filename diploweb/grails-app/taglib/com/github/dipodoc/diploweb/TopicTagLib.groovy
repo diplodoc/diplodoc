ca8fe338eb2247b9be2c4b987976107424530475
yaroslav.yermilov <yermilov17@gmail.com>
2015-03-25 19:05:52 +0200

    improve topic showing

--- diploweb/grails-app/taglib/com/github/dipodoc/diploweb/TopicTagLib.groovy
+++ diploweb/grails-app/taglib/com/github/dipodoc/diploweb/TopicTagLib.groovy
@@ -7,8 +7,6 @@ class TopicTagLib {
     static String SPAN_OPEN = '<span class="property-value" aria-labelledby="train_topics-label">'
     static String SPAN_CLOSE = '</span>'
 
-    // static defaultEncodeAs = [ taglib: 'html' ]
-
     static namespace = 'diplo'
 
     def topics = { attrs, body ->
@@ -16,9 +14,25 @@ class TopicTagLib {
 
         topics = topics.collectMany(this.&unroll) as Set
 
-        topics.each {
+        while (!topics.empty) {
+            Topic leaf = topics.find { Topic leafCandidate ->
+                topics.find({ Topic childCandidate -> childCandidate.parent == leafCandidate }) == null
+            }
+
+            Topic iter = leaf
+
             out << SPAN_OPEN
-            out << topicToHtml(it)
+
+            while (iter != null) {
+                topics.remove(iter)
+                out << topicToHtml(iter)
+
+                iter = iter.parent
+                if (iter != null) {
+                    out << ' <- '
+                }
+            }
+
             out << SPAN_CLOSE
         }
     }
