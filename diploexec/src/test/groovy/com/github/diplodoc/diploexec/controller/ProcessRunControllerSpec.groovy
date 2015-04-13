deb2839febdeaf2f2ff48528ae64a6fe0aaa0778
yaroslav.yermilov <yermilov17@gmail.com>
2015-04-13 14:41:26 +0300

    de-ignore test

--- diploexec/src/test/groovy/com/github/diplodoc/diploexec/controller/ProcessRunControllerSpec.groovy
+++ diploexec/src/test/groovy/com/github/diplodoc/diploexec/controller/ProcessRunControllerSpec.groovy
@@ -1,10 +1,8 @@
 package com.github.diplodoc.diploexec.controller
 
-import com.github.diplodoc.diplobase.domain.mongodb.diploexec.ProcessRun
 import com.github.diplodoc.diplobase.domain.mongodb.diploexec.ProcessRunParameter
 import com.github.diplodoc.diploexec.Diploexec
 import org.bson.types.ObjectId
-import spock.lang.Ignore
 import spock.lang.Specification
 
 /**
@@ -16,15 +14,11 @@ class ProcessRunControllerSpec extends Specification {
 
     ProcessRunController processRunController = new ProcessRunController(diploexec: diploexec)
 
-    @Ignore
     def 'void run(ProcessRun processRun)'() {
-        given:
-            List parameters = [ new ProcessRunParameter(key: 'key-1', value: 'value-1'), new ProcessRunParameter(key: 'key-2', value: 'value-2') ]
-
         when:
-            processRunController.run('111111111111111111111111', parameters)
+            processRunController.run('111111111111111111111111', [])
 
         then:
-            1 * diploexec.run(new ObjectId('111111111111111111111111'), parameters)
+            1 * diploexec.run(new ObjectId('111111111111111111111111'), [])
     }
 }
