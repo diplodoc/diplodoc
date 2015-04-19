088e7108c48234b14098035f58ed141fc353c251
yaroslav.yermilov <yermilov17@gmail.com>
2015-04-19 12:58:24 +0300

    extract text from office docs

---
+++ diplocore/src/main/groovy/com/github/diplodoc/diplocore/modules/knu/TextExtractor.groovy
@@ -0,0 +1,49 @@
+package com.github.diplodoc.diplocore.modules.knu
+
+import com.github.diplodoc.diplobase.domain.mongodb.diplodata.Doc
+import com.github.diplodoc.diplobase.repository.mongodb.diplodata.DocRepository
+import com.github.diplodoc.diplocore.services.AuditService
+import com.github.diplodoc.diplocore.services.RawDataService
+import groovy.util.logging.Slf4j
+import org.bson.types.ObjectId
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.ResponseStatus
+
+/**
+ * @author yaroslav.yermilov
+ */
+@Controller
+@RequestMapping('/text-extractor')
+@Slf4j
+class TextExtractor {
+
+    @Autowired
+    DocRepository docRepository
+
+    @Autowired
+    AuditService auditService
+
+    @Autowired
+    RawDataService rawDataService
+
+    @RequestMapping(value = '/doc/{id}/text', method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    def extractText(@PathVariable('id') String docId) {
+        auditService.runMethodUnderAudit('knu.TextExtractor', 'extractText') { module, moduleMethod, moduleMethodRun ->
+            moduleMethodRun.parameters = [ 'docId': docId ]
+
+            Doc doc = docRepository.findOne new ObjectId(docId)
+
+            doc.meaningText = rawDataService.extractText(doc.binary)
+
+            docRepository.save doc
+
+            [ 'moduleMethodRun': moduleMethodRun ]
+        }
+    }
+}
--- diplocore/src/main/groovy/com/github/diplodoc/diplocore/services/RawDataService.groovy
+++ diplocore/src/main/groovy/com/github/diplodoc/diplocore/services/RawDataService.groovy
@@ -1,8 +1,11 @@
 package com.github.diplodoc.diplocore.services
 
+import org.apache.commons.io.IOUtils
 import org.apache.tika.config.TikaConfig
 import org.apache.tika.io.TikaInputStream
 import org.apache.tika.metadata.Metadata
+import org.apache.tika.parser.AutoDetectParser
+import org.apache.tika.sax.BodyContentHandler
 import org.springframework.stereotype.Service
 
 /**
@@ -17,5 +20,14 @@ class RawDataService {
         Metadata metadata = new Metadata()
         metadata.set(Metadata.RESOURCE_NAME_KEY, uri)
         tikaConfig.getDetector().detect(TikaInputStream.get(rawData), metadata).toString()
+    }
+
+    String extractText(byte[] rawData) {
+        AutoDetectParser autoDetectParser = new AutoDetectParser()
+        BodyContentHandler bodyContentHandler = new BodyContentHandler()
+        Metadata metadata = new Metadata()
+        autoDetectParser.parse(new ByteArrayInputStream(rawData), bodyContentHandler, metadata)
+
+        bodyContentHandler.toString()
     }
 }
