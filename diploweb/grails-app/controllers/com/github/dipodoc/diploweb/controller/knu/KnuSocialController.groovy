52001a588de9b9b9bd4405de23ff5a1ebd5d131d
yaroslav.yermilov <yermilov17@gmail.com>
2015-04-25 22:33:28 +0300

    show beautiful socials

--- diploweb/grails-app/controllers/com/github/dipodoc/diploweb/controller/knu/KnuSocialController.groovy
+++ diploweb/grails-app/controllers/com/github/dipodoc/diploweb/controller/knu/KnuSocialController.groovy
@@ -13,8 +13,11 @@ class KnuSocialController {
 
     static allowedMethods = [ delete: 'DELETE' ]
 
-    def list(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
+    def list() {
+        params.max = 20
+        params.sort = 'publishTime'
+        params.order = 'desc'
+
         def knuSocials = Doc.where { knu == 'social' }
 
         respond knuSocials.list(params), model: [ docInstanceCount: knuSocials.count() ]
