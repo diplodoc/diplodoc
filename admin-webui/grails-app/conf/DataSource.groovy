2df9ef9c9218ce79e4a69086a19357a3040dd902
yaroslav.yermilov <yermilov17@gmail.com>
2015-06-20 15:41:47 +0300

    grails mongodb configuration for shared environments

--- admin-webui/grails-app/conf/DataSource.groovy
+++ admin-webui/grails-app/conf/DataSource.groovy
@@ -1,7 +1,22 @@
-grails {
-    mongo {
-        host = 'localhost'
-        port = 27017
-        databaseName = 'diplodata'
+environments {
+    development {
+        grails {
+            mongo {
+                host = 'localhost'
+                port = 27017
+                databaseName = 'diplodata'
+            }
+        }
+    }
+    production {
+        grails {
+            mongo {
+                databaseName = 'diplodata'
+                host = System.getProperty 'mongodb_host'
+                port = System.getProperty 'mongodb_port'
+                username = System.getProperty 'mongodb_user'
+                password = System.getProperty 'mongodb_password'
+            }
+        }
     }
 }
