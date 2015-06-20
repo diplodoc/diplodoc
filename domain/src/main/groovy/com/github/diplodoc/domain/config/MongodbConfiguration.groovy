9e34b5d5209a38cdf06cc1ac69f73ca6f9851ed3
yaroslav.yermilov <yermilov17@gmail.com>
2015-06-20 15:46:35 +0300

    mongodb configuration for shared environments

--- domain/src/main/groovy/com/github/diplodoc/domain/config/MongodbConfiguration.groovy
+++ domain/src/main/groovy/com/github/diplodoc/domain/config/MongodbConfiguration.groovy
@@ -2,6 +2,7 @@ package com.github.diplodoc.domain.config
 
 import com.mongodb.Mongo
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.authentication.UserCredentials
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
@@ -19,11 +20,16 @@ class MongodbConfiguration extends AbstractMongoConfiguration {
 
     @Override
     Mongo mongo() throws Exception {
-        new Mongo()
+        new Mongo(System.getProperty('mongodb_host'), System.getProperty('mongodb_port'))
     }
 
     @Override
     String getMappingBasePackage() {
         'com.github.diplodoc.domain.mongodb'
+    }
+
+    @Override
+    protected UserCredentials getUserCredentials() {
+        new UserCredentials(System.getProperty('mongodb_user'), System.getProperty('mongodb_password'))
     }
 }
