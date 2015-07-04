4cf9cf5ea1f8cdbfccf6c9388296b28e1c7216cb
yaroslav.yermilov <yermilov17@gmail.com>
2015-07-04 15:34:49 +0300

    clean grails code

--- admin-webui/grails-app/init/com/github/dipodoc/webui/admin/Application.groovy
+++ admin-webui/grails-app/init/com/github/dipodoc/webui/admin/Application.groovy
@@ -1,9 +1,10 @@
-package admin.webui
+package com.github.dipodoc.webui.admin
 
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 
 class Application extends GrailsAutoConfiguration {
+
     static void main(String[] args) {
         GrailsApp.run(Application, args)
     }
