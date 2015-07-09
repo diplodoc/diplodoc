e375cb85ca151b1913b0c9ae672fb4aaa80e3b66
yaroslav.yermilov <yermilov17@gmail.com>
2015-07-09 17:53:23 +0300

    first rough implementation of regular process run

--- orchestration/src/main/groovy/com/github/diplodoc/orchestration/Orchestrator.groovy
+++ orchestration/src/main/groovy/com/github/diplodoc/orchestration/Orchestrator.groovy
@@ -6,7 +6,9 @@ import com.github.diplodoc.domain.repository.mongodb.orchestration.ProcessReposi
 import com.github.diplodoc.domain.repository.mongodb.orchestration.ProcessRunRepository
 import groovy.util.logging.Slf4j
 import org.bson.types.ObjectId
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler
+
+import java.util.concurrent.TimeUnit
 
 /**
  * @author yaroslav.yermilov
@@ -14,7 +16,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 @Slf4j
 class Orchestrator {
 
-    ThreadPoolTaskExecutor threadPool
+    ThreadPoolTaskScheduler scheduler
     ProcessRepository processRepository
     ProcessRunRepository processRunRepository
 
@@ -22,7 +24,17 @@ class Orchestrator {
         ProcessRun processRun = processRunRepository.save new ProcessRun(processId: processId, parameters: parameters)
 
         log.info "starting process ${processRun}..."
-        threadPool.execute(new ProcessCall(this, processRun))
+        scheduler.execute(new ProcessCall(this, processRun))
+
+        return processRun.id
+    }
+
+    ObjectId run(ObjectId processId, long delay) {
+        ProcessRun processRun = processRunRepository.save new ProcessRun(processId: processId, parameters: [])
+        Date startTime = new Date(System.currentTimeMillis() + delay)
+
+        log.info "scheduling process ${processRun} to start at ${startTime}..."
+        scheduler.schedule(new ProcessCall(this, processRun), startTime)
 
         return processRun.id
     }
@@ -46,12 +58,22 @@ class Orchestrator {
                 event.processRun.exitStatus = 'SUCCEED'
                 event.processRun.endTime = event.time
                 processRunRepository.save event.processRun
+
+                def scheduling = findSchedulingPeriod(getProcess(event.processRun.processId))
+                if (scheduling != null) {
+                    run(event.processRun.processId, scheduling.period)
+                }
             break
 
             case ProcessCallEvent.Type.PROCESS_RUN_FAILED:
                 event.processRun.exitStatus = 'FAILED'
                 event.processRun.endTime = event.time
                 processRunRepository.save event.processRun
+
+                def scheduling = findSchedulingPeriod(getProcess(event.processRun.processId))
+                if (scheduling != null) {
+                    run(event.processRun.processId, scheduling.period)
+                }
             break
 
             default:
@@ -99,5 +121,26 @@ class Orchestrator {
         new GroovyShell(binding).evaluate(processListenDefinition)
 
         return inputFor
+    }
+
+    Map findSchedulingPeriod(Process process) {
+        Map scheduling
+
+        String processSchedulingDefinition = process.definition.readLines().findAll({ String line -> line.startsWith('start') }).join('\n')
+        Binding binding = new Binding()
+        binding.start = { Map parameters ->
+            scheduling = [:]
+            scheduling.period = parameters.remove 'every'
+            scheduling.parameters = parameters
+        }
+        Integer.metaClass.propertyMissing = {String name ->
+            TimeUnit timeUnit = TimeUnit.valueOf(name.toUpperCase())
+            if (timeUnit != null) {
+                return timeUnit.toMillis(delegate)
+            }
+        }
+        new GroovyShell(binding).evaluate(processSchedulingDefinition)
+
+        return scheduling
     }
 }
