d0d53073001d14bd619aec38f6125faf88aa1088
yaroslav.yermilov <yermilov17@gmail.com>
2015-08-06 14:25:35 +0300

    add feeder tests

--- modules-java/src/test/groovy/com/github/diplodoc/modules/client/FeederSpec.groovy
+++ modules-java/src/test/groovy/com/github/diplodoc/modules/client/FeederSpec.groovy
@@ -1,11 +1,13 @@
 package com.github.diplodoc.modules.client
 
+import com.github.diplodoc.domain.mongodb.User
 import com.github.diplodoc.domain.mongodb.data.Doc
 import com.github.diplodoc.domain.mongodb.orchestration.Module
 import com.github.diplodoc.domain.mongodb.orchestration.ModuleMethod
 import com.github.diplodoc.domain.mongodb.orchestration.ModuleMethodRun
 import com.github.diplodoc.domain.repository.mongodb.data.DocRepository
 import com.github.diplodoc.modules.services.AuditService
+import com.github.diplodoc.modules.services.SecurityService
 import org.bson.types.ObjectId
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
@@ -19,10 +21,11 @@ class FeederSpec extends Specification {
 
     DocRepository docRepository = Mock(DocRepository)
     AuditService auditService = Mock(AuditService)
+    SecurityService securityService = Mock(SecurityService)
 
-    Feeder feeder = new Feeder(docRepository: docRepository, auditService: auditService)
+    Feeder feeder = new Feeder(docRepository: docRepository, auditService: auditService, securityService: securityService)
 
-    def 'def feed(Integer page, Integer size) {'() {
+    def 'def feed(Integer page, Integer size)'() {
         when:
             1 * auditService.runMethodUnderAudit('client.Feeder', 'feed', _) >> { it ->
                 Module module = new Module()
@@ -37,12 +40,14 @@ class FeederSpec extends Specification {
                 new Doc(id: new ObjectId('222222222222222222222222'), uri: 'uri-2', title: 'title-2', publishTime: 'time-2', description: 'description-2')
             ])
 
+            securityService.authenticate('provider', 'type', 'token') >> new User(id: new ObjectId('333333333333333333333333'))
+
         then:
-            Map actual = feeder.feed(paramPage, paramSize)
+            Map actual = feeder.feed('provider', 'type', 'token', paramPage, paramSize)
 
         expect:
             actual.keySet().size() == 2
-            actual['moduleMethodRun'].parameters == [ 'page': paramPage, 'size': paramSize ]
+            actual['moduleMethodRun'].parameters == [ 'page': paramPage, 'size': paramSize, userId: '333333333333333333333333' ]
             actual['result'] == [
                 [ 'id': '111111111111111111111111', 'url': 'uri-1', 'title': 'title-1', 'time': 'time-1', 'description': 'description-1' ],
                 [ 'id': '222222222222222222222222', 'url': 'uri-2', 'title': 'title-2', 'time': 'time-2', 'description': 'description-2' ]
@@ -54,5 +59,16 @@ class FeederSpec extends Specification {
             2         | null      | 2              | 20
             null      | 30        | 0              | 30
             1         | 50        | 1              | 50
+    }
+
+    def 'def feed(Integer page, Integer size) - authentication failed'() {
+        when:
+            securityService.authenticate('provider', 'type', 'token') >> null
+
+        then:
+            def actual = feeder.feed('provider', 'type', 'token', null, null)
+
+        expect:
+            actual == []
     }
 }
